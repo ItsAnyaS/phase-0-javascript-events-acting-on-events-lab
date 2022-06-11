e7a396edd9218f49a29e602b51d400700e1d2b5a
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-06-11 17:59:43 -0400

    commit tree 5cd6bd91eed7f2f353915c2f2b42b5a3b5235815

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.right.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.right = `${right - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', ()=>{
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    } else {
+        moveDodgerRight()
+    }
+})
